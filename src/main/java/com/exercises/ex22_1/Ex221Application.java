47c7274236c5f476b409d477eda4ab2097e459e0
Agnieszka Majak <agamajak@wp.pl>
2020-06-27 13:50:09 +0200

    Initial commit

---
+++ src/main/java/com/exercises/ex22_1/Ex221Application.java
@@ -0,0 +1,13 @@
+package com.exercises.ex22_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex221Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex221Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/exercises/ex22_1/Ex221ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.ex22_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex221ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
